a94d815b8da35af55277ca4904067809fc7ea97b
Kaise <130371620+Takahashi-Kaisei@users.noreply.github.com>
2026-02-01 20:37:28 +0900

    Update solution for rnn-step-forward

--- rnn-step-forward/rnn-step-forward.py
+++ rnn-step-forward/rnn-step-forward.py
@@ -3,6 +3,7 @@ import numpy as np
 def rnn_step_forward(x_t, h_prev, Wx, Wh, b):
     """
     Returns: h_t of shape (H,)
+    @だと、np.dotと挙動が異なるケースがあるので注意。
     """
     h_t = np.tanh(np.dot(x_t, Wx) + np.dot(h_prev, Wh) + b)
     return h_t
