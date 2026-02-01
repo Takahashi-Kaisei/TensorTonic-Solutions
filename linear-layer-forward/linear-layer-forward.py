95355c669be1b1ed40ea07ec4355d4cb8223e1a2
Kaise <130371620+Takahashi-Kaisei@users.noreply.github.com>
2026-02-01 21:54:30 +0900

    Update solution for linear-layer-forward

--- linear-layer-forward/linear-layer-forward.py
+++ linear-layer-forward/linear-layer-forward.py
@@ -11,15 +11,11 @@ def linear_layer_forward(X, W, b):
     for i in range(len(W[0])):
         W_T.append([j[i] for j in W])
 
-    y = []
-    for X_i in X:
-        y_i = []
-        for W_j, b_j in zip(W_T, b):
-            y_ij = 0
-            for X_ik, W_kj in zip(X_i, W_j):
-                y_ij += X_ik * W_kj
-            y_ij = y_ij + b_j
-            y_i.append(y_ij)
-        y.append(y_i)
-
+    y = [
+        [
+            sum(
+                [X_ik * W_kj for X_ik, W_kj in zip(X_i, W_j)]
+            ) + b_j for W_j, b_j in zip(W_T, b)
+        ] for X_i in X
+    ]
     return y
